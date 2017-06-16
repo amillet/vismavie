6c2c286d9e232cdfa7cb3fccc76d6c3373b5e046
Alexandre Millet <alexandre.millet@wengo.com>
2017-06-16 13:23:48 +0200

    add wall page

--- wall.php
+++ wall.php
@@ -21,7 +21,7 @@
                     <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> il y a 30 minutes</small></p>
                 </div>
                 <div class="timeline-body">
-                    <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/GNYa0O8VYRg" frameborder="0" allowfullscreen></iframe>
                 </div>
             </div>
         </li>
